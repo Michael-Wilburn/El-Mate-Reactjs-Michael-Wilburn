295dcaf6e8e22a779dd4fc0826885bbd243e99f3
Michael Wilburn <michael-mw@hotmail.com>
2022-07-15 19:59:11 -0300

    config cases

---
+++ src/components/CardWidget/CardWidget.js
@@ -0,0 +1,12 @@
+import logo from './mate.png'
+import './CardWidget.css'
+
+const CardWidget = () => {
+    return (
+        <div className="cardWidget">
+            <img src={logo} alt="logo"/>
+        </div>
+    )
+}
+
+export default CardWidget
